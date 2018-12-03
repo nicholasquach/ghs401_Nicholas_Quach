ae3d3c46d00e839d59fd169c550cd33a40e2366d
Nicholas Quach <nicholas.qua@lvjusd.k12.ca.us>
2018-12-03 13:10:50 -0800

    my first command

---
+++ RandomStuff.java
@@ -0,0 +1,10 @@
+public class RandomStuff
+{
+    public static void main(){
+        
+        //cast a random number to an integer
+        int age = (int) (Math.random()*7 + 13);
+        
+        System.out.printIn("The number is "+ age + "!");
+    }
+}
